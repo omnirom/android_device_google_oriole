e89914b70edf9e24d061e8842fbcb30d3f3fde18
maxwen <max.weninger@gmail.com>
2022-12-13 21:41:39 +0100

    oriole: update BUILD_FINGERPRINT for TQ1A.221205.011
    Change-Id: I3f630310d6563475fd9f44fe8988e83d6959051d

--- omni_oriole.mk
+++ omni_oriole.mk
@@ -30,11 +30,10 @@ PRODUCT_SYSTEM_NAME := oriole
 PRODUCT_SYSTEM_MANUFACTURER := Google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="oriole-user 13 TP1A.221105.002 9080065 release-keys"
+    PRIVATE_BUILD_DESC="oriole-user 13 TQ1A.221205.011 9244662 release-keys"
 
-# TODO - could use PRODUCT_OVERRIDE_FINGERPRINT
 # defined in vendor/google_devices/oriole/proprietary/BoardConfigVendor.mk
-BUILD_FINGERPRINT := google/oriole/oriole:13/TP1A.221105.002/9080065:user/release-keys
+BUILD_FINGERPRINT := $(PRODUCT_OVERRIDE_FINGERPRINT)
 
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
