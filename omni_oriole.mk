33695c1e45b7e90b4bdcbbe876f52a9a3f5ba95c
maxwen <max.weninger@gmail.com>
2023-10-24 15:52:08 +0200

    oriole: add building wireless charging hal
    Change-Id: I1a3ddac8a3fec375915e7e5603dcf867df872e13

--- omni_oriole.mk
+++ omni_oriole.mk
@@ -62,3 +62,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     device/google/raviole/the_experiences.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/the_experiences.xml
 
+TARGET_PREBUILT_KERNEL := device/google/raviole-kernel/Image.lz4
+
+# wireless_charger HAL service
+include device/google/gs-common/wireless_charger/wireless_charger.mk
