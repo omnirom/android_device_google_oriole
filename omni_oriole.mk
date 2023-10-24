6d80be33598797e5990dc549710db9238e1e13cf
maxwen <max.weninger@gmail.com>
2023-10-24 15:52:08 +0200

    oriole: set TARGET_PREBUILT_KERNEL
    Change-Id: I1a3ddac8a3fec375915e7e5603dcf867df872e13

--- omni_oriole.mk
+++ omni_oriole.mk
@@ -62,3 +62,5 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     device/google/raviole/the_experiences.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/the_experiences.xml
 
+TARGET_PREBUILT_KERNEL := device/google/raviole-kernel/Image.lz4
+
