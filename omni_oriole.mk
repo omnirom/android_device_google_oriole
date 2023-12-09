837558b050afa5b13eb8372c3405af238931cf66
maxwen <max.weninger@gmail.com>
2023-12-09 02:34:00 +0100

    oriole: update for UQ1A.231205.015
    update fingerprint
    add permissions_com.google.android.apps.camera.services.xml
    remove manifest pin to r2 grahics repos
    
    Change-Id: I7135713046667436ff827e9a0f4a84bfe7d7e785

--- omni_oriole.mk
+++ omni_oriole.mk
@@ -31,10 +31,10 @@ PRODUCT_BRAND := google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=oriole \
-    PRIVATE_BUILD_DESC="oriole-user 14 UP1A.231005.007 10754064 release-keys"
+    PRIVATE_BUILD_DESC="oriole-user 14 UQ1A.231205.015 11084887 release-keys"
 
 # defined in vendor/google_devices/oriole/proprietary/device-vendor.mk
-BUILD_FINGERPRINT := google/oriole/oriole:14/UP1A.231005.007/10754064:user/release-keys
+BUILD_FINGERPRINT := google/oriole/oriole:14/UQ1A.231205.015/11084887:user/release-keys
 
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
@@ -58,6 +58,10 @@ PRODUCT_PACKAGES += \
 # euicc from stock
 PRODUCT_COPY_FILES += \
     device/google/raviole/permissions/permissions_com.google.android.euicc.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/permissions_com.google.android.euicc.xml
+
+# Pixel Camera Services / Camera extensions
+PRODUCT_COPY_FILES += \
+    device/google/raviole/permissions/permissions_com.google.android.apps.camera.services.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/permissions_com.google.android.apps.camera.services.xml
 
 # For Google Camera
 PRODUCT_COPY_FILES += \
