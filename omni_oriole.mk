e21c136e8c7adfb50f83cc9cf83a9add00555e15
maxwen <max.weninger@gmail.com>
2025-01-02 12:48:55 +0100

    oriole: update build fingerprint AP4A.241205.013
    Change-Id: Id5d2e84e904493d2624ef5c8f6c49fdd6e04bdce

--- omni_oriole.mk
+++ omni_oriole.mk
@@ -30,8 +30,8 @@ PRODUCT_MODEL := Pixel 6
 PRODUCT_BRAND := google
 
 PRODUCT_BUILD_PROP_OVERRIDES := \
-    BuildDesc="oriole-user 15 AP3A.241005.015 12366759 release-keys" \
-    BuildFingerprint=google/oriole/oriole:15/AP3A.241005.015/12366759:user/release-keys \
+    BuildDesc="oriole-user 15 AP4A.241205.013 12621605 release-keys" \
+    BuildFingerprint=google/oriole/oriole:15/AP4A.241205.013/12621605:user/release-keys \
     DeviceProduct=oriole
 
 PRODUCT_RESTRICT_VENDOR_FILES := false
